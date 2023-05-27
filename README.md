f5162c17458a3c78f8ed54f35720363e2ddce1d7
Matias Lo Castro <matias.locastro@hotmail.com>
2023-05-27 18:01:34 -0300

    Add README.md and scraper.py files

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# python-scraper
-Python Web Scraper
+# Python-Scraper
+
+This program is a simple web scraper. The program is written in python 3. 
+To start the program you will need to type 
+
+``` python scraper.py ```
+
+After you hit 'enter' the program will interpret the source-code in 'scraper.py'.
+
+In order to get the result on your email, you will have to edit the 'FROM' and 'TO' fields.
+And also put the correct pass for the email sender.
---
+++ scraper.py
@@ -0,0 +1,78 @@
+import requests #http requests
+
+from bs4 import BeautifulSoup #webscraping
+
+import smtplib
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+import datetime
+now = datetime.datetime.now()
+
+
+content = '' # email content
+
+
+
+
+
+def extract_news(url):
+    print('Extracting news')
+    temp_content = ''
+    temp_content += ('<b> News:</b>\n'+'<br>'+'-'*50+'<br>')
+    response = requests.get(url)
+    content = response.content
+
+    soup = BeautifulSoup(content, 'html.parser')
+    links = soup.find_all('a')
+    # for link in links:
+    #    links = ''
+    #    links += link.text
+       
+    for i, tag in enumerate(soup.find_all('a')):
+        soup.find_all('//body/main//div//a', attrs={'class':'title'})
+        temp_content += (str(i+1)+' :: '+tag.text + "\n" + '<br>')
+    
+    
+
+    return(temp_content)
+
+temp_content = extract_news('https://thehackernews.com/')
+content += temp_content
+content += ('<br>--------<br>')
+content += ('<br><br>End of Message')
+
+#send email
+
+print('Composing Email...')
+
+# update your email details
+
+SERVER = 'smtp.gmail.com'
+PORT = 587
+FROM = ''
+TO = ''
+PASS = ''
+
+msg = MIMEMultipart()
+
+msg['Subject'] = 'Hacker News ' + ' ' + str(now.day) + '-' + str(now.month) + '-' + str(now.year)
+
+msg['From'] = FROM
+msg['To'] = TO
+
+msg.attach(MIMEText(content, 'html'))
+
+print('Initiating Server')
+
+server = smtplib.SMTP(SERVER, PORT)
+server.set_debuglevel(1)
+server.ehlo()
+server.starttls()
+server.login(FROM, PASS)
+server.sendmail(FROM, TO, msg.as_string())
+
+print('Email Sent')
+
+server.quit()
